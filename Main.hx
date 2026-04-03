a1299512ff53a1b684659437ea2043eec9d0503c
frisk-V3 <260639938+frisk-V3@users.noreply.github.com>
2026-04-03 15:35:15 +0900

    Push

--- Main.hx
+++ Main.hx
@@ -1,1 +1,7 @@
-
+class Main {
+    static function main() {
+        var args = Sys.args();
+        var name = (args.length > 0) ? args[0] : "名無し";
+        Sys.println('こんにちは、${name}さん！');
+    }
+}
